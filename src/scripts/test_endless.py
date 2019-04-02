6ce4b0c59518cb8cfc11e03dd55c5974b6a587d5
danielatdeckers <48836593+danielatdeckers@users.noreply.github.com>
2019-04-02 15:37:26 -0700

    Add files via upload

---
+++ src/scripts/test_endless.py
@@ -0,0 +1,6 @@
+print("\n\n\n Running!!!! \n\n\n")
+index = 0
+while(True):
+    index += 1
+    print(index)
+
---
+++ src/scripts/test_limited.py
@@ -0,0 +1,5 @@
+print("\n\n\n Running!!!! \n\n\n")
+index = 0
+while(index < 5):
+    index += 1
+    print(index)
